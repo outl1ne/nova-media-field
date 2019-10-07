5eb39e693f05a251eb60f9cdd9161df7cb5e0f53
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2019-10-07 11:26:21 +0300

    Fix sidebar menu item

--- resources/views/navigation.blade.php
+++ resources/views/navigation.blade.php
@@ -1,11 +1,9 @@
-<router-link
-        to="/media-library"
-        class="text-white ml-8 no-underline dim"
->
-    <h3 class="cursor-pointer flex items-center font-normal dim text-white mb-6 text-base no-underline">
-        <svg viewBox="0 0 24 24" class="sidebar-icon">
-            <path fill="var(--sidebar-icon)" d="M12,18H6V14H12M21,14V12L20,7H4L3,12V14H4V20H14V14H18V20H20V14M20,4H4V6H20V4Z" />
-        </svg>
-        Media Library
-    </h3>
+<router-link tag="h3" :to="{name: 'media-library'}"
+    class="cursor-pointer flex items-center font-normal dim text-white mb-6 text-base no-underline">
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" class="sidebar-icon">
+        <path fill="var(--sidebar-icon)" d="M4 4h16a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H4a2 2 0 0 1-2-2V6c0-1.1.9-2 2-2zm16 8.59V6H4v6.59l4.3-4.3a1 1 0 0 1 1.4 0l5.3 5.3 2.3-2.3a1 1 0 0 1 1.4 0l1.3 1.3zm0 2.82l-2-2-2.3 2.3a1 1 0 0 1-1.4 0L9 10.4l-5 5V18h16v-2.59zM15 10a1 1 0 1 1 0-2 1 1 0 0 1 0 2z"/>
+    </svg>
+    <span class="sidebar-label">
+        {{ __('Media Library') }}
+    </span>
 </router-link>
