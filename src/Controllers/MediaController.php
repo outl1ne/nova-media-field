bfa2c1ef7dc9470e40fb18201841cff1106990fb
Tarvo Reinpalu <tarvoreinpalu@gmail.com>
2020-02-11 14:28:22 +0200

    Add failsafes to avoid crashing

--- src/Controllers/MediaController.php
+++ src/Controllers/MediaController.php
@@ -11,17 +11,18 @@ use OptimistDigital\MediaField\Requests\StoreMediaRequest;
 
 class MediaController extends Controller
 {
-
-    public function uploadFile(StoreMediaRequest $request) {
+    public function uploadFile(StoreMediaRequest $request)
+    {
         return response()->json(MediaHandler::createFromRequest($request));
     }
 
-    public function findFiles(Request $request) {
-
+    public function findFiles(Request $request)
+    {
         if (is_array($request->get('ids'))) {
-            $ids = implode(',', $request->get('ids'));
-            $media = Media::whereIn('id', $request->get('ids'))
-                ->orderByRaw("FIELD (id, $ids)")
+            $ids =  array_map('trim', array_filter($request->get('ids'), 'is_numeric'));
+            $idsString = implode(',', $ids);
+            $media = Media::whereIn('id', $ids)
+                ->orderByRaw("FIELD (id, $idsString)")
                 ->get();
             return response()->json($media, 200);
         }
@@ -29,29 +30,27 @@ class MediaController extends Controller
         return response()->json(['Media files not found'], 404);
     }
 
-    public function updateFile(Request $request) {
-
+    public function updateFile(Request $request)
+    {
         $media = null;
-        if (is_numeric($request->get('id'))) {
-            $media = Media::whereId($request->get('id'))->firstOrFail();
-        }
+        if (is_numeric($request->get('id'))) $media = Media::whereId($request->get('id'))->firstOrFail();
 
         $media->title = $request->get('title');
         $media->alt = $request->get('alt');
-
         $media->save();
 
         return response()->json($media);
     }
 
-    public function getFiles(Request $request) {
+    public function getFiles(Request $request)
+    {
         $query = Media::query();
 
         if ($request->has('search')) {
-           $query
-            ->where('file_name', 'like', '%' . $request->get('search') . '%')
-            ->orWhere('alt', 'like', '%' . $request->get('search') . '%')
-            ->orWhere('title', 'like', '%' . $request->get('search') . '%');
+            $query
+                ->where('file_name', 'like', '%' . $request->get('search') . '%')
+                ->orWhere('alt', 'like', '%' . $request->get('search') . '%')
+                ->orWhere('title', 'like', '%' . $request->get('search') . '%');
         }
 
         $paginator = $query->latest()->paginate(30);
