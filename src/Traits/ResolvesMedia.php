1c9a79feee8dad31f165673b2d85e985d0607d35
Marttin Notta <marttin@optimistdigital.com>
2021-04-13 12:19:39 +0300

    Update file resolver

--- src/Traits/ResolvesMedia.php
+++ src/Traits/ResolvesMedia.php
@@ -9,16 +9,32 @@ trait ResolvesMedia
     /*
      * @param $filename - Can be path to a file or an URL, uses fopen to read a file
      */
-    public function findExistingMedia($filename) : ?Media
+    /**
+     * @param resource $file
+     * @return Media|null
+     */
+    public function findExistingMedia($file) : ?Media
     {
-        $hash = $this->getFileHash($filename);
+        $hash = $this->getFileHash($file);
         if (!$hash) return null;
         return Media::whereFileHash($hash)->first();
     }
 
-    public function getFileHash($filename) : ?string{
-        $file = fopen($filename, 'r');
-        if (!$file) return null;
+    /**
+     * @param resource $file
+     * @return string|null
+     */
+    public function getFileHash($file) : ?string{
+        if (!$file || !is_resource($file)) return null;
         return md5(fread($file, 100000000));
+    }
+
+    public function getFileHashFromPath(string $filepath) : ?Media {
+        $file = fopen($filepath, 'r');
+        if ($file) {
+            $existingMedia = $instance->findExistingMedia($file);
+            if ($existingMedia) return $existingMedia;
+        }
+        return null;
     }
 }
